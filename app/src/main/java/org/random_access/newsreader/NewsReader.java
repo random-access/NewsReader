1f94854a6df18dfe30494ebeb6e6477625526761
Monika Schrenk <moni@random-access.org>
2015-09-06 23:39:14 +0200

    changed acra report type to user-friendly dialog

--- app/src/main/java/org/random_access/newsreader/NewsReader.java
+++ app/src/main/java/org/random_access/newsreader/NewsReader.java
@@ -7,8 +7,14 @@ import org.acra.annotation.*;
 @ReportsCrashes(
         //formUri = "http://www.backendofyourchoice.com/reportpath"
         mailTo = "reports@random-access.org",
-        mode = ReportingInteractionMode.TOAST,
-        resToastText = R.string.crash_toast_text,
+        mode = ReportingInteractionMode.DIALOG,
+        resToastText = R.string.crash_toast_text, // optional, displayed as soon as the crash occurs, before collecting data which can take a few seconds
+        resDialogText = R.string.crash_dialog_text,
+        resDialogIcon = R.drawable.ic_crash, //optional. default is a warning sign
+        resDialogTitle = R.string.crash_dialog_title, // optional. default is your application name
+        resDialogCommentPrompt = R.string.crash_dialog_comment_prompt, // optional. When defined, adds a user text field input with this text resource as a label
+        //resDialogEmailPrompt = R.string.crash_user_email_label, // optional. When defined, adds a user email text entry with this text resource as label. The email address will be populated from SharedPreferences and will be provided as an ACRA field if configured.
+        resDialogOkToast = R.string.crash_dialog_ok_toast, // optional. displays a Toast message when the user accepts to send a report.
         logcatArguments = { "-t", "100", "-v", "long", "ActivityManager:I", "NewsReader:D", "*:S" }
 )
 
